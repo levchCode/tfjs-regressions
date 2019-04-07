620cf12668518b875028a99afa887a4c1efaf3d7
BSOD_levch <levch98@mail.ru>
2019-04-07 12:14:19 +0300

    New stuff

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let a, b, c, n, mn1, ttab
 let ctx;
 let lineX = [];
 
-let optimizer = tf.train.adam(0.05)
+let optimizer = tf.train.adamax(0.5)
 
 function preload() {
   dataset = loadTable('data.csv','csv','header')
@@ -27,22 +27,22 @@ function setup() {
     y_vals.push(y)
   }
 
-  x_min = Math.min(...x_vals)
-  x_max = Math.max(...x_vals)
-  y_min = Math.min(...y_vals)
-  y_max = Math.max(...y_vals)
+  // x_min = Math.min(...x_vals)
+  // x_max = Math.max(...x_vals)
+  // y_min = Math.min(...y_vals)
+  // y_max = Math.max(...y_vals)
 
-  d = []
-  for (i = 0; i < n; i++) {
-    x = (x_vals[i] - x_min)/(x_max-x_min)
-    y = (y_vals[i] - y_min)/(y_max-y_min)
-    d.push({x:x, y:y})
-    x_vals[i] = x + 0.000001
-    y_vals[i] = y + 0.000001
-  }
+  // d = []
+  // for (i = 0; i < n; i++) {
+  //   x = (x_vals[i] - x_min)/(x_max-x_min)
+  //   y = (y_vals[i] - y_min)/(y_max-y_min)
+  //   d.push({x:x, y:y})
+  //   x_vals[i] = x + 0.000001
+  //   y_vals[i] = y + 0.000001
+  // }
 
   for (i = 0; i < n; i++) {
-    lineX.push(i/5)
+    lineX.push(i*3)
   }
   
   a = tf.variable(tf.scalar(0));
@@ -60,27 +60,70 @@ function run()
   c = tf.variable(tf.scalar(0));
 }
 
+function dev2(x) {
+  return x.sub(x.mean()).square().sum().div(n)
+}
+
 function stats(x, y, y_p)
 {
 
-    let residual_dev = y_p.sub(y).square().sum().div(n)
+    let residual_dev = y.sub(y_p).square().sum().div(n)
 
-    let res_dev_t = y_p.sub(y).square().sum().div(nm1)
+    let r2 = tf.scalar(1).sub(residual_dev.div(dev2(y))) 
 
-    //let A = y.sub(y_p).div(y).abs().sum().div(n)
+    let A = y.sub(y_p).div(y).abs().sum().div(n).mul(tf.scalar(100))
 
-    let mb = tf.sqrt(res_dev_t.div(x.sub(x.mean()).square().sum()))
-    let ma = tf.sqrt((res_dev_t.mul(x.square().sum())).div(n.mul(x.sub(x.mean()).square().sum())))
+    let F_l = y_p.sub(y.mean()).square().sum().div(y.sub(y_p).square().sum()).mul(n.sub(tf.scalar(2))).dataSync()[0]
+    let F_n = r2.div(tf.scalar(1).sub(r2)).mul(n.sub(tf.scalar(2))).dataSync()[0]
+
+    let s_res = y.sub(y_p).square().sum().div(n.sub(tf.scalar(2)))
+
+    let mb = s_res.div(dev2(x).sqrt().mul(n.sqrt()))
+    let ma = s_res.mul(x.square().sum().sqrt().div(dev2(x).sqrt().mul(n)))
+    let mr = tf.scalar(1).sub(r2).div(n.sub(tf.scalar(2)))
 
     let ta = a.div(ma).dataSync()
     let tb = b.div(mb).dataSync()
+    let tr = r2.div(mr).dataSync()
 
-    //document.getElementById("a").innerHTML = "A = " + A.dataSync()[0].toFixed(4) + " %"
-    document.getElementById("rsd").innerHTML = "Остаточная дисперсия = " + residual_dev.dataSync()[0].toFixed(4)
+    let a_l = a.sub(tf.scalar(parseFloat(document.getElementById("t").value)).mul(ma))
+    let a_h = a.add(tf.scalar(parseFloat(document.getElementById("t").value)).mul(ma))
+
+    let b_l = b.sub(tf.scalar(parseFloat(document.getElementById("t").value)).mul(mb))
+    let b_h = b.add(tf.scalar(parseFloat(document.getElementById("t").value)).mul(mb))
+
+    document.getElementById("rsd").innerHTML = "σ<sub>ост</sub> = " + residual_dev.dataSync()[0].toFixed(4)
+    document.getElementById("sr").innerHTML = "S<sub>ост</sub> = " + s_res.dataSync()[0].toFixed(4)
+    document.getElementById("r2").innerHTML = "r<sup>2</sup> = " + r2.dataSync()[0].toFixed(4)
+    document.getElementById("a").innerHTML = "A = " + A.dataSync()[0].toFixed(4)
+    document.getElementById("F").innerHTML = "F = " + F_l.toFixed(4)
+    document.getElementById("F2").innerHTML = "F = " + F_n.toFixed(4)
     document.getElementById("mb").innerHTML = "m<sub>b</sub> = " + mb.dataSync()[0].toFixed(4)
     document.getElementById("ma").innerHTML = "m<sub>a</sub> = " + ma.dataSync()[0].toFixed(4)
+    document.getElementById("mr").innerHTML = "m<sub>r</sub> = " + mr.dataSync()[0].toFixed(4)
     document.getElementById("tb").innerHTML = "t<sub>b</sub> = " + tb[0].toFixed(4)
     document.getElementById("ta").innerHTML = "t<sub>a</sub> = " + ta[0].toFixed(4)
+    document.getElementById("tr").innerHTML = "t<sub>r</sub> = " + tr[0].toFixed(4)
+    document.getElementById("ai").innerHTML = "Доверительный интервал (a) = [" + a_l.dataSync()[0].toFixed(4) + "; " + a_h.dataSync()[0].toFixed(4) + "]"
+    document.getElementById("bi").innerHTML = "Доверительный интервал (b) = [" + b_l.dataSync()[0].toFixed(4) + "; " + b_h.dataSync()[0].toFixed(4) + "]"
+
+    if ( F_l >= parseFloat(document.getElementById("f").value))
+    {
+      document.getElementById("F").style.cssText = "color: green"
+    }
+    else
+    {
+      document.getElementById("F").style.cssText = "color: red"
+    }
+
+    if (F_n >= parseFloat(document.getElementById("f").value))
+    {
+      document.getElementById("F2").style.cssText = "color: green"
+    }
+    else
+    {
+      document.getElementById("F2").style.cssText = "color: red"
+    }
 
     if (tb >= parseFloat(document.getElementById("t").value))
     {
@@ -98,6 +141,15 @@ function stats(x, y, y_p)
     else
     {
       document.getElementById("ta").style.cssText = "color: red"
+    }
+
+    if (tr >= parseFloat(document.getElementById("t").value))
+    {
+      document.getElementById("tr").style.cssText = "color: green"
+    }
+    else
+    {
+      document.getElementById("tr").style.cssText = "color: red"
     }
 }
 
@@ -202,6 +254,4 @@ function draw() {
   }
 );
 ys.dispose();
-
-  console.log(tf.memory().numTensors);
 }
